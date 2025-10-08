ae4eed32cf455f995fb8adf64e05430525708ebd
sunghun0422 <godlee0422@gmail.com>
2025-10-08 10:10:10 +0200

    Create calendar.js

---
+++ server/routes/calendar.js
@@ -0,0 +1,14 @@
+import { ok } from "../lib/util.js";
+
+export default async (req, res) => {
+  const date = req.query.date || new Date().toISOString().slice(0,10);
+  // TODO: Google Calendar OAuth 연동
+  return res.json(ok({
+    date,
+    items: [
+      { time: "09:00", title: "팀 스탠드업", location: "Meet", note: "" },
+      { time: "12:30", title: "점심 미팅", location: "TF 근처", note: "신과장/이과장" },
+      { time: "16:00", title: "문서 검토", location: "Office", note: "보고서 초안" }
+    ]
+  }));
+};
