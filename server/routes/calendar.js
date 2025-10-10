62ec22f9b80351d02e7ea098be0f42809c829513
sunghun0422 <godlee0422@gmail.com>
2025-10-10 13:46:40 +0200

    Update calendar.js

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,18 +1,14 @@
-// server/routes/calendar.js
-import { ok } from "../lib/util.js";
+const { ok } = require("../lib/util.js");
 
-export default async function calendar(req, res) {
+module.exports = function calendar(req, res) {
   const date = req.query.date || new Date().toISOString().slice(0, 10);
 
-  // TODO: 추후 Google Calendar OAuth 연동 예정
-  return res.json(
-    ok({
-      date,
-      items: [
-        { time: "09:00", title: "팀 스탠드업", location: "Meet", note: "" },
-        { time: "12:30", title: "점심 미팅", location: "TF 근처", note: "신과장/이과장" },
-        { time: "16:00", title: "문서 검토", location: "Office", note: "보고서 초안" }
-      ]
-    })
-  );
-}
+  res.json(ok({
+    date,
+    items: [
+      { time: "09:00", title: "팀 스탠드업", location: "Meet", note: "" },
+      { time: "12:30", title: "점심 미팅", location: "TF 근처", note: "신과장/이과장" },
+      { time: "16:00", title: "문서 검토", location: "Office", note: "보고서 초안" }
+    ]
+  }));
+};
