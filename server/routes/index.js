dc9486bf263eaec9f5de6dc4206c26125b81e105
sunghun0422 <godlee0422@gmail.com>
2025-10-10 13:42:31 +0200

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,3 @@
-// server/routes/index.js
-
 import express from "express";
 import calendar from "./calendar.js";
 import summarize from "./summarize.js";
@@ -7,7 +5,6 @@ import docs from "./docs.js";
 
 const router = express.Router();
 
-// 📌 각각의 경로를 라우팅
 router.get("/calendar-daily", calendar);
 router.post("/summarize", summarize);
 router.get("/docs", docs);
