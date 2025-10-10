b51dae223176a81c3f27acb8063f2665c92bdc94
sunghun0422 <godlee0422@gmail.com>
2025-10-10 16:21:27 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
-mkdir api
-echo "// serverless entry" > api/index.js
-git add api/index.js
-git commit -m "add api entry"
-git push
+// /api/index.js
+// Vercel serverless entry: wrap Express app in a handler function.
+
+const app = require("../server/index");
+
+module.exports = (req, res) => {
+  return app(req, res);
+};
