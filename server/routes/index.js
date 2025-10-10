e56f2c755839f076d5d5affc331e18f1a3b2256c
sunghun0422 <godlee0422@gmail.com>
2025-10-10 13:47:02 +0200

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,8 @@
-import express from "express";
-import calendar from "./calendar.js";
-import summarize from "./summarize.js";
-import docs from "./docs.js";
+const express = require("express");
+const calendar = require("./calendar.js");
 
 const router = express.Router();
 
 router.get("/calendar-daily", calendar);
-router.post("/summarize", summarize);
-router.get("/docs", docs);
 
-export default router;
+module.exports = router;
