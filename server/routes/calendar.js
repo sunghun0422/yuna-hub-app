bb0f0279fa11b9d4bdb5e8a2bf9493e84bf5c3a9
sunghun0422 <godlee0422@gmail.com>
2025-10-10 14:46:43 +0200

    Update calendar.js

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,14 +1,34 @@
-const { ok } = require("../lib/util.js");
+export default function (req, res) {
+  const { date } = req.query;
 
-module.exports = function calendar(req, res) {
-  const date = req.query.date || new Date().toISOString().slice(0, 10);
+  if (!date) {
+    return res.status(400).json({ ok: false, error: "Missing date parameter" });
+  }
 
-  res.json(ok({
+  const data = {
+    ok: true,
     date,
     items: [
-      { time: "09:00", title: "팀 스탠드업", location: "Meet", note: "" },
-      { time: "12:30", title: "점심 미팅", location: "TF 근처", note: "신과장/이과장" },
-      { time: "16:00", title: "문서 검토", location: "Office", note: "보고서 초안" }
+      {
+        time: "09:00",
+        title: "팀 스탠드업",
+        location: "Meet",
+        note: ""
+      },
+      {
+        time: "12:30",
+        title: "점심 미팅",
+        location: "TF 근처",
+        note: "신과장/이과장"
+      },
+      {
+        time: "16:00",
+        title: "문서 검토",
+        location: "Office",
+        note: "보고서 초안"
+      }
     ]
-  }));
-};
+  };
+
+  res.status(200).json(data);
+}
