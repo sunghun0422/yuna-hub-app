5f0cfa88bee37d9117d1967b3c8d941bb9b0bebf
sunghun0422 <godlee0422@gmail.com>
2025-10-10 13:10:06 +0200

    Update util.js

--- server/lib/util.js
+++ server/lib/util.js
@@ -1,1 +1,14 @@
-export const ok = (data = {}) => ({ ok: true, ...data });
+// server/lib/util.js
+
+function ok(data) {
+  return { ok: true, ...data };
+}
+
+function fail(message = "Something went wrong.") {
+  return { ok: false, error: message };
+}
+
+module.exports = {
+  ok,
+  fail
+};
