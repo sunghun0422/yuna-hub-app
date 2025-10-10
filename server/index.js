b9cf588309e9f220dc834a5d87c0d14544ef2dd1
sunghun0422 <godlee0422@gmail.com>
2025-10-10 13:47:27 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,33 +1,26 @@
-// server/index.js
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const routes = require("./routes/index.js");
-const calendarRoutes = require("./routes/calendar.js");
+
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 미들웨어
 app.use(cors());
 app.use(express.json());
 
-// API 라우트
 app.use("/api", routes);
-app.use("/", calendarRoutes);
 
-// health check
+app.use(express.static(path.join(__dirname, "../public")));
+app.use("/.well-known", express.static(path.join(__dirname, "../public/.well-known")));
+
 app.get("/healthz", (req, res) => {
   res.status(200).json({ ok: true });
 });
 
-// 정적 파일 제공
-app.use(express.static(path.join(__dirname, "../public")));
-app.use("/.well-known", express.static(path.join(__dirname, "../public/.well-known")));
-
 app.get("/", (req, res) => {
-  res.send("💗 Yuna Hub App is running successfully!");
+  res.send("Yuna Hub App is running!");
 });
 
 module.exports = app;
