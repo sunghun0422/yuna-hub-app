4926d9e2b7bcd3265ac17eb1a659f911d827da24
sunghun0422 <godlee0422@gmail.com>
2025-10-13 15:54:09 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import serverless from "serverless-http";
+
 import healthRouter from "../server/routes/health.js";
 import memoryRouter from "../server/routes/memory.js";
 import storybookRouter from "../server/routes/storybook.js";
@@ -7,18 +8,15 @@ import gmailRouter from "../server/routes/gmail.js";
 import calendarRouter from "../server/routes/calendar.js";
 
 const app = express();
-
-// JSON parsing middleware
 app.use(express.json());
 
-// Routes
+// Router 연결
 app.use("/api/health", healthRouter);
 app.use("/api/memory", memoryRouter);
 app.use("/api/storybook", storybookRouter);
 app.use("/api/gmail", gmailRouter);
 app.use("/api/calendar", calendarRouter);
 
-// Root endpoint (optional check)
 app.get("/", (req, res) => {
   res.json({ ok: true, message: "Yuna Hub 💖 root working" });
 });
