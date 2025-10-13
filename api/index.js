20ded86d4bd338054eaddfbaecf06d2fe95ffbf9
sunghun0422 <godlee0422@gmail.com>
2025-10-13 13:41:18 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
+import express from "express";
 import serverless from "serverless-http";
-import app from "../server/index.js";
 
-const handler = serverless(app);
-export default handler;
+const app = express();
+
+app.get("/healthz", (req, res) => {
+  res.json({ ok: true, message: "Yuna Hub 💖 running smoothly" });
+});
+
+export default app;
+export const handler = serverless(app);
