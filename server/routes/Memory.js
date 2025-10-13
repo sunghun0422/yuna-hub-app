712021071236e992654415bc8efd66a95f312fb8
sunghun0422 <godlee0422@gmail.com>
2025-10-13 16:18:21 +0200

    Update Memory.js

--- server/routes/Memory.js
+++ server/routes/Memory.js
@@ -1,9 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-router.post("/memory/new", (req, res) => {
+router.post("/new", (req, res) => {
   const data = req.body || {};
-  console.log("📥 Memory received:", data);
   res.json({ ok: true, message: "Memory saved successfully", data });
 });
 
