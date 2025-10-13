f3288ebb2d5177da7cf14825a30feb1f70528067
sunghun0422 <godlee0422@gmail.com>
2025-10-13 15:55:09 +0200

    Update health.js

--- server/routes/health.js
+++ server/routes/health.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ ok: true, message: "Yuna Hub 💗 health check passed" });
+  res.json({ ok: true, message: "Yuna Hub 💖 health check passed" });
 });
 
 export default router;
