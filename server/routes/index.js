7a2eb23354de1864d4ca23904daaf839cbdff574
sunghun0422 <godlee0422@gmail.com>
2025-10-13 12:23:49 +0200

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,16 @@
 import express from "express";
-import summarize from "./summarize.js";
-import memory from "./Memory.js";
-import storybook from "./Storybook.js";
-import gmail from "./gmail.js";
-import calendar from "./calendar.js"; // ✅ 캘린더 추가
+import summarizeRouter from "./summarize.js";
+import memoryRouter from "./Memory.js";
+import storybookRouter from "./Storybook.js";
+import gmailRouter from "./gmail.js";
+import calendarRouter from "./calendar.js";
 
 const router = express.Router();
 
-router.use("/summarize", summarize);
-router.use("/memory", memory);
-router.use("/storybook", storybook);
-router.use("/gmail", gmail);
-router.use("/calendar", calendar); // ✅ 추가 완료
+router.use("/summarize-url", summarizeRouter);
+router.use("/memory", memoryRouter);
+router.use("/storybook", storybookRouter);
+router.use("/gmail", gmailRouter);
+router.use("/calendar", calendarRouter);
 
 export default router;
